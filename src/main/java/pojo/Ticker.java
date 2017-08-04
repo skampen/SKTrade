91820555c7eac804596a5c35c147647a183447a6
s.kampen <sakekampen@gmail.com>
2017-08-04 14:43:06 +0200

    init

---
+++ src/main/java/pojo/Ticker.java
@@ -0,0 +1,113 @@
+package pojo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "TICKER")
+public class Ticker {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "TICKER_SEQ")
+    @javax.persistence.SequenceGenerator(
+            name = "TICKER_SEQ",
+            sequenceName = "TICKER_SEQ",
+            allocationSize = 1
+    )
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "CURRENCYPAIR")
+    private String currencyPair;
+
+    @Column(name = "LAST")
+    private String last;
+
+    @Column(name = "LOWESTASK")
+    private String lowestAsk;
+
+    @Column(name = "HIGHESTBID")
+    private String highestBid;
+
+    @Column(name = "PERCENTCHANGE")
+    private String percentChange;
+
+    @Column(name = "BASEVOLUME")
+    private String baseVolume;
+
+    @Column(name = "QUOTEVOLUME")
+    private String quoteVolume;
+
+    public Ticker() {
+    }
+
+    public Ticker(String currencyPair, String last, String lowestAsk, String highestBid, String percentChange, String baseVolume, String quoteVolume) {
+        this.currencyPair = currencyPair;
+        this.last = last;
+        this.lowestAsk = lowestAsk;
+        this.highestBid = highestBid;
+        this.percentChange = percentChange;
+        this.baseVolume = baseVolume;
+        this.quoteVolume = quoteVolume;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCurrencyPair() {
+        return currencyPair;
+    }
+
+    public void setCurrencyPair(String currencyPair) {
+        this.currencyPair = currencyPair;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public String getLowestAsk() {
+        return lowestAsk;
+    }
+
+    public void setLowestAsk(String lowestAsk) {
+        this.lowestAsk = lowestAsk;
+    }
+
+    public String getHighestBid() {
+        return highestBid;
+    }
+
+    public void setHighestBid(String highestBid) {
+        this.highestBid = highestBid;
+    }
+
+    public String getPercentChange() {
+        return percentChange;
+    }
+
+    public void setPercentChange(String percentChange) {
+        this.percentChange = percentChange;
+    }
+
+    public String getBaseVolume() {
+        return baseVolume;
+    }
+
+    public void setBaseVolume(String baseVolume) {
+        this.baseVolume = baseVolume;
+    }
+
+    public String getQuoteVolume() {
+        return quoteVolume;
+    }
+
+    public void setQuoteVolume(String quoteVolume) {
+        this.quoteVolume = quoteVolume;
+    }
+}
---
+++ src/main/java/runner/ManageTicker.java
@@ -0,0 +1,122 @@
+package runner;
+
+import java.util.List;
+import java.util.Date;
+import java.util.Iterator;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import pojo.Ticker;
+
+@SuppressWarnings("unchecked")
+public class ManageTicker {
+    private static SessionFactory factory;
+    public static void main(String[] args) {
+        try{
+            factory = new Configuration().configure().buildSessionFactory();
+        }catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+        ManageTicker MT = new ManageTicker();
+
+      /* Add few ticker records in database */
+        Integer empID1 = MT.addTicker("USDT_BTC", "2738.76231502", "2738.76231501", "2737.31000003", "0.01888425","12895541.80779788","4739.76312700");
+        Integer empID2 = MT.addTicker("USDT_BTC", "2738.51999999", "2738.51999999", "2735.33000001", "0.01690259","12690304.12270306","4663.28437851");
+        Integer empID3 = MT.addTicker("USDT_BTC", "2738.51999999", "2738.51999999", "2735.43000002", "0.01539488","12615288.84520339","4635.35600351");
+
+      /* List down all the tickers */
+        MT.listTickers();
+
+      /* Update ticker's records */
+//        MT.updateTicker(empID1, 5000);
+
+      /* Delete an ticker from the database */
+//        MT.deleteTicker(empID2);
+
+      /* List down new list of the tickers */
+        MT.listTickers();
+    }
+
+    /* Method to CREATE an ticker in the database */
+    private Integer addTicker(String currencyPair, String last, String lowestAsk, String highestBid, String percentChange, String baseVolume, String quoteVolume){
+        Session session = factory.openSession();
+        Transaction tx = null;
+        Integer tickerID = null;
+        try{
+            tx = session.beginTransaction();
+            Ticker ticker = new Ticker(currencyPair, last, lowestAsk, highestBid, percentChange, baseVolume, quoteVolume);
+
+            tickerID = (Integer) session.save(ticker);
+            tx.commit();
+        }catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        }finally {
+            session.close();
+        }
+        return tickerID;
+    }
+    /* Method to  READ all the tickers */
+    private void listTickers( ){
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try{
+            tx = session.beginTransaction();
+            List tickers = session.createQuery("FROM Ticker").list();
+            for(Ticker ticker : (List<Ticker>) tickers){
+                System.out.print("Currency pair: " + ticker.getCurrencyPair());
+                System.out.print("Last: " + ticker.getLast());
+                System.out.println("Lowest ask: " + ticker.getLowestAsk());
+                System.out.println("Highest bid: " + ticker.getHighestBid());
+                System.out.println("Percent change: " + ticker.getPercentChange());
+                System.out.println("Base volume: " + ticker.getBaseVolume());
+                System.out.println("Quote volume: " + ticker.getQuoteVolume());
+            }
+            tx.commit();
+        }catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+        }finally {
+            session.close();
+        }
+    }
+    /* Method to UPDATE salary for an ticker */
+//    public void updateTicker(Integer TickerID, int salary ){
+//        Session session = factory.openSession();
+//        Transaction tx = null;
+//        try{
+//            tx = session.beginTransaction();
+//            Ticker ticker =
+//                    (Ticker)session.get(Ticker.class, TickerID);
+//            ticker.setSalary( salary );
+//            session.update(ticker);
+//            tx.commit();
+//        }catch (HibernateException e) {
+//            if (tx!=null) tx.rollback();
+//            e.printStackTrace();
+//        }finally {
+//            session.close();
+//        }
+//    }
+//    /* Method to DELETE an ticker from the records */
+//    public void deleteTicker(Integer TickerID){
+//        Session session = factory.openSession();
+//        Transaction tx = null;
+//        try{
+//            tx = session.beginTransaction();
+//            Ticker ticker =
+//                    (Ticker)session.get(Ticker.class, TickerID);
+//            session.delete(ticker);
+//            tx.commit();
+//        }catch (HibernateException e) {
+//            if (tx!=null) tx.rollback();
+//            e.printStackTrace();
+//        }finally {
+//            session.close();
+//        }
+//    }
+}
